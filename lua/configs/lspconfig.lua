49ba4108d3e9e2ac104d4d62ceac12d8540ce307
daonm <daobilionusd@gmail.com>
2026-01-21 10:40:08 +0700

    Add gitignore and add schemas for yamlls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -13,13 +13,20 @@ local servers = {
   "helm_ls",
 }
 
-vim.lsp.config('yamlls', {
+vim.lsp.config("yamlls", {
   settings = {
     yaml = {
+      validate = true,
+      schemaStore = {
+        enable = true,
+        url = "https://www.schemastore.org/api/json/catalog.json",
+      },
       schemas = {
-        kubernetes = {"k8s/*.yaml", "manifests/*.yaml"},
+        kubernetes = { "k8s**.yaml", "kube*/*.yaml", "k8s*/*.yaml", "manifests/*.yaml" },
         ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-        ["http://json.schemastore.org/kustomization"] = "kustomization.yaml",
+        ["https://json.schemastore.org/kustomization.json"] = "kustomization.{yml,yaml}",
+        ["https://raw.githubusercontent.com/docker/compose/master/compose/config/compose_spec.json"] = "docker-compose*.{yml,yaml}",
+        ["https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/argoproj.io/application_v1alpha1.json"] = "argocd-application.yaml",
       },
     },
   },
